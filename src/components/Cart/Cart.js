b81b4c581d7ec74a89cdd9704c7c4a88fc0d9c08
Matt Logan <mlogan5212@gmail.com>
2021-04-23 17:08:50 -0500

    Add form to cart. Show conditionally when user clicks order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
@@ -20,6 +23,10 @@ const Cart = (props) => {
     cartCtx.addItem(cartItem);
   };
 
+  const orderHandler = () => {
+    setIsCheckout(true);
+  }
+
   const cartItems = (<ul className={classes['cart-items']}>
     {cartCtx.items.map((item) => (
       <CartItem
@@ -33,6 +40,13 @@ const Cart = (props) => {
     ))}
   </ul>)
 
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
+      {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+    </div>
+  )
+
   return (
     <Modal onClick={props.onHideCart}>
       {cartItems}
@@ -40,10 +54,8 @@ const Cart = (props) => {
       <span>Total Amount</span>
       <span>{totalAmount}</span>
     </div>
-    <div className={classes.actions}>
-      <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-      {hasItems && <button className={classes.button}>Order</button>}
-    </div>
+    {isCheckout && <Checkout onCancel={props.onHideCart} />}
+    {!isCheckout && modalActions}
   </Modal>
   )
 };
